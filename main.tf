095138ebaf8416ad7bfa3c477c6fd83e44243111
Eric Muir <eric@ebdm.dev>
2021-03-05 11:44:47 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,43 @@
+data "aws_route53_zone" "zone" {
+  name         = var.zone_name
+  private_zone = false
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.host
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.zone.zone_id
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+output "arn" {
+	value = aws_acm_certificate.cert.arn
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "host" {
+	type = string
+}
+
+variable "zone_name" {
+	type = string
+}
+
+variable "region" {
+	default = "eu-west-2"
+	type = string
+}
